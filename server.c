4936200c1a785d5f59afa127ee215bcbf80c5610
ematsi02 <ematsi02@cs.ucy.ac.cy>
2017-04-09 18:14:42 +0300

    Update server.c

--- server.c
+++ server.c
@@ -5,84 +5,115 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+
+int openCon(int sock,struct sockaddr_in self, struct sockaddr_in server, struct sockaddr *serverptr,int serverlen,int port){
+	  if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+	    perror("socket");
+	    exit(1);
+	  }
+
+	  bzero(&self, sizeof(self));
+	  server.sin_family = AF_INET; /*Internet domain*/
+	  server.sin_addr.s_addr = htonl(INADDR_ANY); /*My Internet address*/
+	  server.sin_port = htons(port);
+	  serverptr = (struct sockaddr *) &server;
+	  serverlen = sizeof(server);
+
+
+	if (bind(sock, serverptr, serverlen) < 0)
+	    { /* Bind socket to an address */
+	     perror("bind");
+	     exit(1);
+	    }
+	 /*Listen for connections*/
+	  if (listen(sock, 5) < 0) { /*5 max requests in queue*/
+	    perror("listen");
+	    exit(1);
+	  }
+
+	  return sock;
+
+}
 
 int main(int argc, char *argv[]) {
   int sock, newsock, serverlen, clientlen;
-  char buf [256], *action;
-  int port = 30000;
+  int port = 8080;
   struct sockaddr_in self, server, client;
   struct sockaddr *serverptr, *clientptr;
   struct hostent *rem;
+  char buf[256];
+  char word[30];
+  char* method=NULL;
   char *answerCode="HTTP/1.1 ";
-  char *serverName="\r\nServer: ";
+  char *serverName="\r\nServer: myServer ";
   char *contentLen="\r\nContent-Length: ";
   char *connection="\r\nConnection: ";
   char *contentType="\r\nContent-Type: ";
-  //char* buf501="HTTP/1.1 501 Not Implemented\r\nServer:%s"+"\r\nContent-Length:%d"+"\r\nConnection:%s"+"\r\nContent-Type:%s"+"\r\n\r\n";
-  char* buf501="HTTP/1.1 501 Not Implemented\r\nServer: myServer\r\nContent-Length: 24\r\nConnection: close\r\nContent-Type: text/plain\r\n\r\nMethod not implemented!\n";
+  char* buf501="HTTP/1.1 501 Not Implemented\r\nServer: myServer\r\nContent-Length: 24\r\nConnection: keep-alive\r\nContent-Type: text/plain\r\n\r\nMethod not implemented!\n";
   char* content501="\r\n\r\nMethod not implemented!\n";
-  
+  char mybuf[500];
 
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
-    exit(1);
-  }
+int sk= openCon(sock,self,server,serverptr,serverlen,port);
 
-  server.sin_family = AF_INET; 
-  server.sin_addr.s_addr = htonl(INADDR_ANY); 
-  server.sin_port = htons(port); 
-  serverptr = (struct sockaddr *) &server;
-  serverlen = sizeof(server);
 
-  if (bind(sock, serverptr, serverlen) < 0) {
-      perror("bind");
-    exit(1);
-  }
 
-  if (listen(sock, 5) < 0) { 
-    perror("listen");
-    exit(1);
-  }
 
   while(1) {
     clientptr = (struct sockaddr *) &client;
     clientlen = sizeof(client);
 
-    if ((newsock = accept(sock, clientptr, &clientlen)) < 0) {
+    /*Accept connection*/
+    if ((newsock = accept(sk, clientptr, &clientlen)) < 0) {
       perror("accept");
       exit(1);
     }
-    
+
     if ((rem = gethostbyaddr((char *) &client.sin_addr.s_addr, sizeof(client.sin_addr.s_addr), client.sin_family)) == NULL) {
       herror("gethostbyaddr");
       exit(1);
     }
     printf("Accepted connection from %s\n", rem->h_name);
-    
-    bzero(buf, sizeof buf);
-    if (read(newsock, buf, sizeof buf) < 0) {
-      perror("read");
-      exit(1);
-    }
+
+    bzero(buf, sizeof buf); /* Initialize buffer */
+    if (read(newsock, buf, sizeof buf) < 0){ /* Receive message */
+       perror("read");
+       exit(1);
+     }
     printf("Read string: %s\n", buf);
-    action = strtok(buf, " ");
-    if (strcmp(action, "GET")) {
-      action = strtok(buf, " ");
-    }
-    else if (strcmp(action, "HEAD")) {
-      action = strtok(buf, " ");
-    }
-    else if (strcmp(action, "DELETE")) {
-      action = strtok(buf, " ");
-    }
-    else {
-      // send(newsock,strcat(answerCode,"Not Implemented"),strlen(answerCode)+15,0);
-	    write(newsock,buf501,strlen(buf501));
-    }
+
+   method=strtok(buf," ");
+   printf("%s\n",method);
+   if(strcmp(method,"GET")==0){
+	   printf("%s\n",method);
+	   printf("mpika sto get");
+   }
+   else if (strcmp(method,"HEAD")==0){
+	   printf("Read string: %s\n", buf);
+	   printf("mpika sto head");
+   }
+   else if (strcmp(method,"DELETE")==0){
+	   printf("Read string: %s\n", buf);
+	   printf("mpika sto delete");
+   }
+   else {
+	   printf("mpika sto else");
+
+	  sprintf(mybuf,"%s501 Not Implemented%s%s%d%skeep-alive%stext/plain%s",answerCode,serverName,contentLen,strlen(content501),connection,contentType,content501);
+	   write(newsock,mybuf,strlen(mybuf));
+//	   send(newsock,serverName,strlen(serverName),0);
+//	   send(newsock,strcat(contentLen,(char*)strlen(content501)),strlen(contentLen)+2,0);
+//	   send(newsock,strcat(connection,"keep-alive"),strlen(connection)+10,0);
+//	   send(newsock,strcat(contentType,"text/plain"),strlen(contentType)+10,0);
+
+
+	   //write(newsock,buf501,strlen(buf501));
+
+   }
+
+    close(newsock);
   }
-   close(newsock);
   close(sock);
   return 0;
 }
