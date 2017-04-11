49a1b195ce994ac05fa97bcdfc51e0e5b9f63c1c
sgrego02 <sgrego02@cs.ucy.ac.cy>
2017-04-11 11:13:47 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,120 +1,66 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "queue.h"
 
-#define TRUE  1
-#define FALSE	0
-
-/* a link in the queue, holds the info and point to the next Node*/
-typedef struct {
-    int info;
-} DATA;
-
-typedef struct Node_t {
-    DATA data;
-    struct Node_t *prev;
-} NODE;
-
-/* the HEAD of the Queue, hold the amount of node's that are in the queue*/
-typedef struct Queue {
-    NODE *head;
-    NODE *tail;
-    int size;
-    int limit;
-} Queue;
-
-Queue *ConstructQueue(int limit);
-void DestructQueue(Queue *queue);
-int Enqueue(Queue *pQueue, NODE *item);
-NODE *Dequeue(Queue *pQueue);
-int isEmpty(Queue* pQueue);
-
-Queue *ConstructQueue(int limit) {
+Queue *ConstructQueue() {
     Queue *queue = (Queue*) malloc(sizeof (Queue));
     if (queue == NULL) {
         return NULL;
     }
-    if (limit <= 0) {
-        limit = 65535;
-    }
-    queue->limit = limit;
     queue->size = 0;
     queue->head = NULL;
     queue->tail = NULL;
-
     return queue;
 }
 
 void DestructQueue(Queue *queue) {
-    NODE *pN;
-    while (!isEmpty(queue)) {
-        pN = Dequeue(queue);
-        free(pN);
+    NODE *p;
+    while (isEmpty(queue) != 0) {
+        p = Dequeue(queue);
+        free(p);
     }
     free(queue);
 }
 
-int Enqueue(Queue *pQueue, NODE *item) {
-    /* Bad parameter */
-    if ((pQueue == NULL) || (item == NULL)) {
-        return FALSE;
-    }
-    // if(pQueue->limit != 0)
-    if (pQueue->size >= pQueue->limit) {
-        return FALSE;
-    }
-    /*the queue is empty*/
-    item->prev = NULL;
-    if (pQueue->size == 0) {
-        pQueue->head = item;
-        pQueue->tail = item;
-
-    } else {
-        /*adding item to the end of the queue*/
-        pQueue->tail->prev = item;
-        pQueue->tail = item;
-    }
-    pQueue->size++;
-    return TRUE;
+int Enqueue(Queue *pQueue, pthread_t tid) {
+   NODE *p = NULL;
+   p = (NODE *)malloc(sizeof(NODE));
+   if (p == NULL) {
+	perror("malloc");
+	exit(1);
+   }
+   p->tid = tid;
+   p->next = NULL;
+   if (pQueue->size == 0)
+     pQueue->head = pQueue->tail = p;
+   else {
+     pQueue->tail->next = p;
+     pQueue->tail = p;
+   }
+   (pQueue->size)++;
+   return 0;
 }
 
-NODE * Dequeue(Queue *pQueue) {
-    /*the queue is empty or bad param*/
-    NODE *item;
-    if (isEmpty(pQueue))
-        return NULL;
-    item = pQueue->head;
-    pQueue->head = (pQueue->head)->prev;
-    pQueue->size--;
-    return item;
+int Dequeue(Queue *pQueue) {
+   NODE *p = NULL;
+   if ((pQueue == NULL) || (pQueue->head == NULL)) {
+	perror("empty queue");
+	exit(1);
+   }
+   p = pQueue->head;
+   pQueue->head = pQueue->head->next;
+   if (pQueue->head == NULL)
+     	pQueue->tail = NULL;
+   free(p);
+   return 0;
 }
 
 int isEmpty(Queue* pQueue) {
     if (pQueue == NULL) {
-        return FALSE;
+        perror("empty queue");
+	exit(1);
     }
     if (pQueue->size == 0) {
-        return TRUE;
+        return 0;
     } else {
-        return FALSE;
+        return -1;
     }
-}
-
-int main() {
-    int i;
-    Queue *pQ = ConstructQueue(7);
-    NODE *pN;
-
-    for (i = 0; i < 9; i++) {
-        pN = (NODE*) malloc(sizeof (NODE));
-        pN->data.info = 100 + i;
-        Enqueue(pQ, pN);
-    }
-
-    while (!isEmpty(pQ)) {
-        pN = Dequeue(pQ);
-        printf("\nDequeued: %d", pN->data);
-        free(pN);
-    }
-    DestructQueue(pQ);
-    return (EXIT_SUCCESS);
 }
